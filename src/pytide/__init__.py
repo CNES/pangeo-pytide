e2c7de0aea46da44aa76ff3ac574049cbc2f6f5a
Frédéric Briol <fbriol@cls.fr>
2019-06-28 18:23:27 +0200

    Documentation update.

--- src/pytide/__init__.py
+++ src/pytide/__init__.py
@@ -1,3 +1,7 @@
+"""
+Tidal constituents analysis
+###########################
+"""
 import numpy
 from . import core
 
@@ -9,6 +13,7 @@ Args:
   epoch (float, optional): Desired UTC time
 """
     pass
+
 
 class Wave(core.Wave):
     """Tidal wave properties"""
@@ -27,16 +32,63 @@ class WaveTable(core.WaveTable):
         return [wave.name() for wave in self]
 
     @staticmethod
-    def harmonic_analysis(h, f, v0u, dtype=None):
-        """Perform tidal harmonic analysis for a given signal h.
+    def harmonic_analysis(h, f, vu, dtype=None):
+        """Harmonic Analysis
 
-        f and v0u are the nodal factor calculated for the time series studied.
+        The harmonic analysis method consists in expressing the ocean tidal
+        variations as a sum of independent constituents accordingly to the
+        tidal potential spectrum. Then the sea surface elevation at a point
+        :math:`(x, y)` and time :math:`t` can be expressed as a linear sum as
+        follow:
 
-        This method returns the real and imaginary part of the
-        constituents defined in nodal factors handled
+        .. math::
+
+            S_{ap} = S_{0}(x, y) + \\sum_{k=0}^n f_{k}(t)S_{k}(x, y)
+            \\times cos [\\omega_{k}t + {v}_{k}(t) + u_{k}(t) - G_{k}(x,y)]
+
+        where:
+
+            * :math:`n` is the number of constituents,
+            * :math:`S_{0}(x, y)` is the mean sea level,
+            * :math:`S_{k}(x, y)` is the amplitude of the constituent of index
+              :math:`k`,
+            * :math:`G_{k}(x, y)` is the phase lag relative to Greenwich time,
+            * :math:`w_{k}` is the angular frequency of the constituent of
+              index :math:`k`,
+            * :math:`v_{k}` is the astronomical argument at time :math:`t`,
+            * :math:`f_{k}(t)` is the nodal correction coefficient applied to
+              the amplitude of the constituent of index :math:`k`,
+            * :math:`u_{k}(t)` is the nodal correction coefficient applied to
+              the phase of the constituent of index :math:`k`.
+
+        The a priori analysis spectrum includes the most important astronomical
+        constituents in the Darwin development, completed by Shureman in 1958,
+        and many non-linear waves. The definition of tidal constants and
+        astronomical arguments is taken from FES2014 tidal prediction software
+        (lien bitbucket ) and a complete definition of waves is also available
+        in Shureman (1958). This spectrum is the most commonly used for
+        harmonic analysis due the simplification given by the nodal correction
+        concept (:math:`f` and :math:`u` coefficients above) which allows
+        dealing with slow motions of the lunar ascending node and reducing the
+        number of constituents in the tidal spectrum.
+
+        More details about this harmonic analysis method can be found in
+        Ponchaut et al. 1999.
+
+        Args:
+            h (numpy.ndarray): Sea level.
+            f (numpy.ndarray): Nodal correction coefficient applied to the
+                amplitude of the constituents analyzed.
+            vu (numpy.ndarray): Astronomical argument at time :math:`t` + the
+                nodal correction coefficient applied to the phase of the
+                constituents analyzed
+
+        Returns:
+            numpy.ndarray: The complex number representing the different
+            reconstructed waves.
         """
         if dtype is None:
-            return core.WaveTable.harmonic_analysis(h, f, v0u)
+            return core.WaveTable.harmonic_analysis(h, f, vu)
         return core.WaveTable.harmonic_analysis.astype(dtype)
 
     @staticmethod
