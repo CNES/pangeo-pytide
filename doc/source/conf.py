e3122086f3db04111b8e225ef5508f117e49c33b
Frédéric Briol <fbriol@cls.fr>
2019-07-01 16:22:40 +0200

    Documentation update.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -47,7 +47,7 @@ if on_rtd:
         ]
     }
 else:
-    html_theme = 'alabaster'
+    html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
