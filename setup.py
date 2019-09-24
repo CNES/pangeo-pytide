242a319802f517d64091cd4ccaf2e3f9d3f7d6b1
Frédéric Briol <fbriol@cls.fr>
2019-09-24 09:34:20 +0200

    Updating package information.

--- setup.py
+++ setup.py
@@ -34,6 +34,21 @@ def execute(cmd):
     return process.stdout.read().decode()
 
 
+def update_meta(path, version):
+    """Updating the version number description in conda/meta.yaml."""
+    with open(path, "r") as stream:
+        lines = stream.readlines()
+    pattern = re.compile(r'{% set version = ".*" %}')
+
+    for idx, line in enumerate(lines):
+        match = pattern.search(line)
+        if match is not None:
+            lines[idx] = '{%% set version = "%s" %%}\n' % version
+
+    with open(path, "w") as stream:
+        stream.write("".join(lines))
+
+
 def revision():
     """Returns the software version"""
     cwd = pathlib.Path().absolute()
@@ -53,8 +68,7 @@ def revision():
                     return match.group(1)
         raise AssertionError()
 
-    # TODO: Delete this test which will be useless after the creation of the
-    # first tag
+    # No tag already registred
     if match is None:
         pattern = re.compile(r'([\w\d]+)(?:-(dirty))?')
         match = pattern.search(stdout)
@@ -66,19 +80,31 @@ def revision():
 
     stdout = execute("git log  %s -1 --format=\"%%H %%at\"" % sha1)
     stdout = stdout.strip().split()
-    date = datetime.datetime.fromtimestamp(int(stdout[1]))
-    sha1 = stdout[0]
+    date = datetime.datetime.utcfromtimestamp(int(stdout[1]))
 
-    # Updating the version number description in "meta.yaml"
+    # This file is not present in the distribution, but only in the GIT
+    # repository of the source code.
     meta = os.path.join(cwd, 'conda', 'meta.yaml')
-    with open(meta, "r") as stream:
+    if os.path.exists(meta):
+        update_meta(meta, version)
+
+    # Updating the version number description for sphinx
+    conf = os.path.join(cwd, 'docs', 'source', 'conf.py')
+    with open(conf, "r") as stream:
         lines = stream.readlines()
-    pattern = re.compile(r'\s+version:\s+(.*)')
+    pattern = re.compile(r'(\w+)\s+=\s+(.*)')
+
     for idx, line in enumerate(lines):
         match = pattern.search(line)
         if match is not None:
-            lines[idx] = '  version: %s\n' % version
-    with open(meta, "w") as stream:
+            if match.group(1) == 'version':
+                lines[idx] = "version = %r\n" % version
+            elif match.group(1) == 'release':
+                lines[idx] = "release = %r\n" % version
+            elif match.group(1) == 'copyright':
+                lines[idx] = "copyright = '(%s, CNES/CLS)'\n" % date.year
+
+    with open(conf, "w") as stream:
         stream.write("".join(lines))
 
     # Finally, write the file containing the version number.
@@ -91,12 +117,11 @@ Get software version information
 
 def release(full: bool = False) -> str:
     """Returns the software version number"""
-    # {sha1}
     result = "{version}"
     if full:
         result += " ({date})"
     return result
-'''.format(sha1=sha1, version=version, date=date.strftime("%d %B %Y")))
+'''.format(version=version, date=date.strftime("%d %B %Y")))
     return version
 
 
@@ -128,7 +153,10 @@ class BuildExt(setuptools.command.build_ext.build_ext):
         eigen_include_dir = os.path.join(sys.prefix, "include", "eigen3")
         if os.path.exists(eigen_include_dir):
             return "-DEIGEN3_INCLUDE_DIR=" + eigen_include_dir
-        eigen_include_dir = os.path.join(sys.prefix, "Library", "include")
+        eigen_include_dir = os.path.join(sys.prefix, "Library", "include",
+                                         "eigen3")
+        if not os.path.exists(eigen_include_dir):
+            eigen_include_dir = os.path.dirname(eigen_include_dir)
         if not os.path.exists(eigen_include_dir):
             raise RuntimeError(
                 "Unable to find the Eigen3 library in the conda distribution "
@@ -140,7 +168,29 @@ class BuildExt(setuptools.command.build_ext.build_ext):
         """Detect if the Python interpreter is part of a conda distribution."""
         result = os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
         if not result:
-            return 'conda-bld' in sysconfig.get_config_var("abs_srcdir")
+            try:
+                # pylint: disable=unused-import
+                import conda
+                # pylint: enable=unused-import
+            except ImportError:
+                result = False
+            else:
+                result = True
+        return result
+
+    def set_cmake_user_options(self):
+        """Sets the options defined by the user."""
+        is_conda = self.is_conda()
+        result = []
+
+        if self.CXX_COMPILER is not None:
+            result.append("-DCMAKE_CXX_COMPILER=" + self.CXX_COMPILER)
+
+        if self.EIGEN3_INCLUDE_DIR is not None:
+            result.append("-DEIGEN3_INCLUDE_DIR=" + self.EIGEN3_INCLUDE_DIR)
+        elif is_conda:
+            result.append(self.eigen())
+
         return result
 
     def build_cmake(self, ext):
@@ -159,18 +209,7 @@ class BuildExt(setuptools.command.build_ext.build_ext):
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir),
             "-DPYTHON_EXECUTABLE=" + sys.executable
-        ]
-
-        if self.CXX_COMPILER is not None:
-            cmake_args.append("-DCMAKE_CXX_COMPILER=" + self.CXX_COMPILER)
-
-        is_conda = self.is_conda()
-
-        if self.EIGEN3_INCLUDE_DIR is not None:
-            cmake_args.append("-DEIGEN3_INCLUDE_DIR=" +
-                              self.EIGEN3_INCLUDE_DIR)
-        elif is_conda:
-            cmake_args.append(self.eigen())
+        ] + self.set_cmake_user_options()
 
         build_args = ['--config', cfg]
 
@@ -181,11 +220,17 @@ class BuildExt(setuptools.command.build_ext.build_ext):
                 cmake_args += ['-DCMAKE_OSX_DEPLOYMENT_TARGET=10.14']
         else:
             cmake_args += [
+                '-G', 'Visual Studio 15 2017',
                 '-DCMAKE_GENERATOR_PLATFORM=x64',
                 '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
                     cfg.upper(), extdir)
             ]
             build_args += ['--', '/m']
+            if self.verbose:
+                build_args += ['/verbosity:n']
+
+        if self.verbose:
+            build_args.insert(0, "--verbose")
 
         os.chdir(str(build_temp))
         self.spawn(['cmake', str(cwd)] + cmake_args)
