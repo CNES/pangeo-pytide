e98761709cb7c98b88feb32b1a3d42aa9e91881e
Frédéric Briol <fbriol@gmail.com>
2020-01-31 18:57:05 +0100

    Release 1.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ copyright = '(2020, CNES/CLS)'
 author = 'CNES/CLS'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.1'
+release = '1.2.2'
 
 # -- General configuration ---------------------------------------------------
 
