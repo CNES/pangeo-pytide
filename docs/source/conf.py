56d1151e23dafce255c6b50233265e02fd16c4cf
Frédéric BRIOL <fbriol@gmail.com>
2021-11-07 14:40:26 +0100

    bump to version 1.2.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,11 @@ import os
 # -- Project information -----------------------------------------------------
 
 project = 'pytide'
-copyright = '(2020, CNES/CLS)'
+copyright = '(2021, CNES/CLS)'
 author = 'CNES/CLS'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.3'
+release = '1.2.4'
 
 # -- General configuration ---------------------------------------------------
 
